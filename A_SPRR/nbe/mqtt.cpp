89326e8e9e29598c06deeca7377a5ab2bda4e42d
AZAbdullah <annuarzainiabdullah68@gmail.com>
2019-10-12 15:35:09 +0800

    new test

---
+++ A_SPRR/nbe/mqtt.cpp
@@ -0,0 +1,18 @@
+/*
+ * mqtt.cpp
+ *
+ *  Created on: Oct 12, 2019
+ *      Author: annuar
+ */
+
+#include "mqtt.h"
+
+mqtt::mqtt() {
+	// TODO Auto-generated constructor stub
+
+}
+
+mqtt::~mqtt() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ A_SPRR/nbe/mqtt.h
@@ -0,0 +1,17 @@
+/*
+ * mqtt.h
+ *
+ *  Created on: Oct 12, 2019
+ *      Author: annuar
+ */
+
+#ifndef NBE_MQTT_H_
+#define NBE_MQTT_H_
+
+class mqtt {
+public:
+	mqtt();
+	virtual ~mqtt();
+};
+
+#endif /* NBE_MQTT_H_ */
